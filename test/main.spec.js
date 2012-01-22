a3e7e057069881205cbdf7a51795771a80aea3e0
Alex Barlow <alexbarlowis@gmail.com>
2012-01-22 23:10:22 +0000

    Add ability to render some input fields and another test for that

--- test/main.spec.js
+++ test/main.spec.js
@@ -2,9 +2,23 @@ var Formed = require('../lib/index');
 
 describe("Formed", function() {
   
-  it("should render an empty form without any fields", function() {
+  it("should render an empty form element without any fields", function() {
     var form = Formed.createForm();
     expect(form).toBe("<form>\n</form>\n");
+  });
+  
+  it("should render a form with inputs", function() {
+    var fields = [
+      {name: 'title', type: 'text', required: null},
+      {name: 'description', type: 'text', required: null}
+    ]
+   var form = Formed.createForm(fields);
+   
+   expect(form).toBe(
+"<form>\n\
+<input name='title' type='text' required>\n\
+<input name='description' type='text' required>\n\
+</form>\n");
   });
   
 });
